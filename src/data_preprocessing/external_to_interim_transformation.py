dfd51c0f2eaf4260e3e21b9db01d9bda22b56a1b
amromeshref <amr.khamees@ejust.edu.eg>
2024-04-08 20:44:10 +0200

    update external_to_interim_transformation.py

--- src/data_preprocessing/external_to_interim_transformation.py
+++ src/data_preprocessing/external_to_interim_transformation.py
@@ -1,13 +1,15 @@
-from src.data_preprocessing.cycles_divider import CyclesDivider
-from src.logger import logging
-from src.exception import CustomException
-import argparse
-import sys
-import os
 # Add the repository directory path to the Python path
 REPO_DIR_PATH = os.path.normpath(os.path.join(
     os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..")))
 sys.path.append(REPO_DIR_PATH)
+
+
+from src.data_preprocessing.cycles_divider import CyclesDivider
+from src.logger import logging
+from src.exception import CustomException
+import sys
+import os
+
 
 
 class ExternalToInterimTransformer(CyclesDivider):
@@ -24,59 +26,63 @@ class ExternalToInterimTransformer(CyclesDivider):
         output:
             None
         """
-        # Define the path to the external data directory
-        external_data_dir = os.path.normpath(os.path.join(
-            os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "data", "external", "self_collected_data"))) + "/"
+        try:
+            # Define the path to the external data directory
+            external_data_dir = os.path.normpath(os.path.join(
+                os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "data", "external", "self_collected_data"))) + "/"
 
-        # Define the path to the interim data directory
-        interim_data_dir = os.path.normpath(os.path.join(
-            os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "data", "interim"))) + "/"
+            # Define the path to the interim data directory
+            interim_data_dir = os.path.normpath(os.path.join(
+                os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "data", "interim"))) + "/"
 
-        # Define the list of criterias
-        criterias = ["criteria_1", "criteria_2", "criteria_3", "criteria_4"]
+            # Define the list of criterias
+            criterias = ["criteria_1", "criteria_2", "criteria_3", "criteria_4"]
 
-        # Iterate over each available exercise
-        for exercise in self.available_exercises:
-            # Define the directory for the exercise data
-            exercise_data_dir = external_data_dir + exercise + "/"
+            # Iterate over each available exercise
+            for exercise in self.available_exercises:
+                # Define the directory for the exercise data
+                exercise_data_dir = external_data_dir + exercise + "/"
 
-            # Iterate over each criteria
-            for criteria in criterias:
-                # Define the directory for the criteria data
-                criteria_data_dir = exercise_data_dir + criteria + "/"
+                # Iterate over each criteria
+                for criteria in criterias:
+                    # Define the directory for the criteria data
+                    criteria_data_dir = exercise_data_dir + criteria + "/"
 
-                # Iterate over each label in the criteria directory
-                for label in os.listdir(criteria_data_dir):
-                    # Iterate over each video file in the label directory
-                    for video_file in os.listdir(criteria_data_dir + label):
-                        # Define the full path to the video file
-                        video_path = criteria_data_dir + label + "/" + video_file
+                    # Iterate over each label in the criteria directory
+                    for label in os.listdir(criteria_data_dir):
+                        # Iterate over each video file in the label directory
+                        for video_file in os.listdir(criteria_data_dir + label):
+                            # Define the full path to the video file
+                            video_path = criteria_data_dir + label + "/" + video_file
 
-                        # Log the start of video processing
-                        logging.info(
-                            "Started processing the video: " + video_path)
+                            # Log the start of video processing
+                            logging.info(
+                                "Started processing the video: " + video_path)
 
-                        # Get the cycles from the video
-                        cycles = self.get_cycles(video_path)
+                            # Get the cycles from the video
+                            cycles = self.get_cycles(video_path)
 
-                        # Get the video name without extension
-                        video_name = self.get_video_name_without_extension(
-                            video_path)
+                            # Get the video name without extension
+                            video_name = self.get_video_name_without_extension(
+                                video_path)
 
-                        # Define the output directory for saving cycles
-                        output_dir = interim_data_dir + exercise + "/" + criteria + "/" + label
+                            # Define the output directory for saving cycles
+                            output_dir = interim_data_dir + exercise + "/" + criteria + "/" + label
 
-                        # Save the cycles as videos in the interim directory
-                        self.save_cycles_as_videos(
-                            cycles, video_name, output_dir)
+                            # Save the cycles as videos in the interim directory
+                            self.save_cycles_as_videos(
+                                cycles, video_name, output_dir)
 
-                        # Log the completion of video processing
-                        logging.info(
-                            "Completed processing the video: " + video_path)
+                            # Log the completion of video processing
+                            logging.info(
+                                "Completed processing the video: " + video_path)
 
-        # Log the completion of data transformation
-        logging.info(
-            "Data transformation from external to interim is completed")
+            # Log the completion of data transformation
+            logging.info(
+                "Data transformation from external to interim is completed")
+        except Exception as e:
+            logging.error("Error: "+str(e))
+            raise CustomException(e, sys)
 
 
 if __name__ == "__main__":
